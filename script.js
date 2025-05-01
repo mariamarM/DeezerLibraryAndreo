9aeda4c24cf32f783eca3a9053f54c49003b5055
mariamarM <mariaandoli@campus.monlau.com>
2025-05-01 20:31:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,210 @@
+//esto es el scroll horizontal con la ruedecita chiquitna del mouse tal 
+document.addEventListener("DOMContentLoaded", () => {
+  const contenedor = document.getElementById("cancionesDeezer");
+
+  if (contenedor) {
+    contenedor.addEventListener("wheel", (e) => {
+      e.preventDefault();
+      contenedor.scrollLeft += e.deltaY;
+    });
+  } 
+});
+//aqui empieza la vaina de la busqueda de las canciones hay q comentarla cuando estas en registros
+  // document.getElementById('formBusqueda').addEventListener("submit", function (event) {
+  //   event.preventDefault(); 
+  
+  //   const nombre = document.getElementById("nombreBanda").value.trim(); 
+  //   const resultado = document.getElementById("cancionesDeezer");
+  //   resultado.textContent = ""; 
+  
+  //   if (nombre === "") {
+  //     resultado.style.color = "red";
+  //     resultado.textContent = "⚠️ Write the name of the band or artist.";
+  //     return;
+  //   }
+  
+  //   buscarCanciones(nombre);
+  // });
+
+
+  //esto es la busqueda del index.html
+  function buscarCanciones(nombre) {
+    const url = `https://api.deezer.com/search?q=${encodeURIComponent(nombre)}`;
+    const resultado = document.getElementById("cancionesDeezer");
+
+    fetch(`https://corsproxy.io/?${url}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const resultados = data.data;
+   
+  
+        if (resultados.length == 0) {
+          resultado.textContent = "🚫 We don't know any song with this name.";
+          resultado.style.color = "red";
+       
+          return;
+        }
+        resultado.style.overflow = "scroll";
+        resultado.innerHTML = resultados
+          .slice(0, 4)
+          .map((song) => `
+            <div class="cancionSingular">
+            <img id="imagen" src="${song.artist.picture_small}" alt="iagen Pequeña de la cancion XS">
+              <h2 id ="nombreArtista">${song.title}</h2>
+              <h3>${song.artist.name}</h3>
+              <audio controls data-song='${JSON.stringify(song)}'>//id="audioControls"
+                <source src="${song.preview}">
+              </audio>
+            </div>
+          `)
+          .join("");
+          
+          const audios = resultado.querySelectorAll("audio");
+          audios.forEach((audio) => {
+            audio.addEventListener("play", () => {
+              const songData = JSON.parse(audio.getAttribute("data-song"));
+        
+              const cancionReproducida = {
+                title: songData.title,
+                artist: songData.artist.name,
+                album: songData.album.title,
+                preview: songData.preview,
+                image: songData.album.cover_medium
+              };
+        
+              
+              let cancionesGuardadas = JSON.parse(localStorage.getItem("cancionesReproducidas")) || [];
+              cancionesGuardadas.push(cancionReproducida);
+              localStorage.setItem("cancionesReproducidas", JSON.stringify(cancionesGuardadas));
+        
+              alert(" Song has been saved", cancionReproducida);
+            });
+          });
+      })
+      .catch((error) => {
+        console.error("Error al buscar la canción", error);
+        document.getElementById("cancionesDeezer").textContent = "Can't upload the song. Check your network connection.";
+      });
+  } 
+
+//REGISTERS
+// function guardarRegistros(){
+//   let guardarlosDatos = [];
+//   let nombreCancionLS = localStorage.getItem('nombreBanda');
+//   document.getElementById('nombreBanda').value = nombreCancionLS;
+
+//   let nombreArtistaLS = localStorage.getItem('nombreArtista');
+//   document.getElementById('nombreArtista').value = nombreArtistaLS;
+
+//   let duracion = audio.duration;
+//   let duracionCancionLS = localStorage.setItem('audioControls', duracion.toString());
+
+//   document.getElementById('audioControls').value = duracionCancionLS;
+
+//   localStorage.setItem('nombreDelArtista', nombreArtistaLS);
+//   localStorage.setItem('nombreDeLaCancion', nombreCancionLS);
+
+//   guardarlosDatos.JSONparse(nombreCancionLS,nombreArtistaLS, duracionCancionLS);
+//   guardarlosDatos.forEach();
+
+// }
+
+//localstorage 2 en la practica
+// aqui se pondran como todas las canciones que se registren pues iran aqui
+document.addEventListener("DOMContentLoaded", () => {
+  const filtroTipo = document.getElementById("filtro-tipo");
+  const contenedorRegistros = document.getElementById("allSongs");
+
+  let canciones = JSON.parse(localStorage.getItem("cancionesReproducidas")) ;
+  if (!canciones || !Array.isArray(canciones)) {
+    console.warn("No hay canciones en localStorage");
+    return; 
+  }
+ 
+  const generos = ["reggae", "jazz", "pop", "electro", "rap", "indie"];
+  canciones.forEach((cancion, indice) => {
+    cancion.tipo = generos[indice % generos.length];
+  });
+
+  
+  
+  function mostrarCanciones(lista) {
+    contenedorRegistros.innerHTML = "";
+    lista.forEach((song) => {
+    contenedorRegistros.innerHTML += `
+      <div class="cancionSingular">
+        <img src="${song.image}" alt="${song.title} album cover">
+        <h2>${song.title}</h2>
+        <h3>${song.artist}</h3>
+        <h4>Album: ${song.album}</h4>
+        
+        <audio controls>
+          <source src="${song.preview}">
+        </audio>
+      </div>
+    `;
+  });
+  }
+
+  mostrarCanciones(canciones);
+
+  filtroTipo.addEventListener("change", () => {
+    const tipoSeleccionado = filtroTipo.value;
+    contenedorRegistros.innerHTML = " ";
+    
+    let cancionesFiltradas = [];
+    // const filtradas = tipoSeleccionado === "todos"
+    // ? canciones
+    // : canciones.filter(c => c.tipo === tipoSeleccionado);
+  //esto es como el if mas simplificado osea es una constante que siempre sera igual
+  //el ? es como si es diferente a eso que tiposeleccionado sera igual a canciones y si no q te lo filtre
+
+
+
+    if (tipoSeleccionado === "todos") {
+      cancionesFiltradas = canciones;
+      //esto es pa q se me vuelva a printar todas las canciones osea q si va a 
+      //jazz q te printe esas y luego cuando vuelva al general q es todos q se vuelva a 
+      //printar porq antes no me lo hacia con esto si 
+      cancionesFiltradas.forEach(song => {
+        contenedorRegistros.innerHTML += `
+          <div class="cancionSingular">
+            <img src="${song.image}" alt="${song.title} album cover">
+            <h2>${song.title}</h2>
+        <h3 >${song.artist}</h3>
+        <h4>Album: ${song.album}</h4>
+        
+        <audio controls>
+          <source src="${song.preview}">
+        </audio>
+          </div>
+        `;
+      });
+    } else {
+      cancionesFiltradas = canciones.filter(c => c.tipo === tipoSeleccionado);
+      cancionesFiltradas.forEach(song => {
+        contenedorRegistros.innerHTML += `
+          <div class="cancionSingular">
+            <img src="${song.image}" alt="${song.title} album cover">
+            <h2>${song.title}</h2>
+        <h3 >${song.artist}</h3>
+        <h4>Album: ${song.album}</h4>
+        
+        <audio controls>
+          <source src="${song.preview}">
+        </audio>
+          </div>
+        `;
+      });
+    }
+
+    
+  });
+});
+
+
+
+
+
+
+//por cada cancion guardada en el foreach usa la funcion mostrarpokemon par enseñarla pero como no la tienes no te funciona
